9ef1ad3f4384ea5561010f3cb5fc705a8c4fd6cb
0lukas <lukas.ceplikas@gmail.com>
2016-11-17 01:39:39 +0200

    Returns temperature from API

--- src/NFQAkademija/WeatherBundle/Provider/YahooWeatherProvider.php
+++ src/NFQAkademija/WeatherBundle/Provider/YahooWeatherProvider.php
@@ -4,6 +4,8 @@ namespace NFQAkademija\WeatherBundle\Provider;
 
 use NFQAkademija\WeatherBundle\Service\Location;
 use NFQAkademija\WeatherBundle\Service\Weather;
+use GuzzleHttp\Client as GuzzleClient;
+use NFQAkademija\WeatherBundle\Service\WeatherException;
 
 class YahooWeatherProvider implements WeatherProviderInterface
 {
@@ -14,6 +16,19 @@ class YahooWeatherProvider implements WeatherProviderInterface
      */
     public function fetch(Location $location): Weather
     {
-        return new Weather(20);
+        $yql_query = 'select item.condition.temp from weather.forecast where woeid in (SELECT woeid FROM geo.places WHERE text="('.$location->getLatitude().','.$location->getLongitude().')") AND u="c"';
+        $yql_url = 'http://query.yahooapis.com/v1/public/yql?q=' . urlencode($yql_query) . '&format=json';
+
+        // Guzzle Request
+        $client   = new GuzzleClient();
+        $response = $client->get($yql_url);
+        $json = json_decode($response->getBody()->getContents());
+
+        if ($json->query->count == 0)
+            throw new WeatherException('Can\'t get temperature');
+
+        $temp = $json->query->results->channel->item->condition->temp;
+
+        return new Weather($temp);
     }
 }
