cf13ac72136f00086d239f6e2429dd6552bbc0bb
Lukas <lukasceplikas@gmail.com>
2016-11-17 00:26:33 +0200

    Generate docblocks for methods

--- src/NFQAkademija/WeatherBundle/Service/Location.php
+++ src/NFQAkademija/WeatherBundle/Service/Location.php
@@ -7,17 +7,33 @@ class Location
     private $longitude;
     private $latitude;
 
+    /**
+     * Location constructor.
+     *
+     * @param float $longitude
+     * @param float $latitude
+     */
     public function __construct(float $longitude, float $latitude)
     {
         $this->longitude = $longitude;
         $this->latitude = $latitude;
     }
 
+    /**
+     * Get longitude
+     *
+     * @return float
+     */
     public function getLongitude(): float
     {
         return $this->longitude;
     }
 
+    /**
+     * Get latitude
+     *
+     * @return float
+     */
     public function getLatitude(): float
     {
         return $this->latitude;
--- src/NFQAkademija/WeatherBundle/Service/Weather.php
+++ src/NFQAkademija/WeatherBundle/Service/Weather.php
@@ -6,11 +6,21 @@ class Weather
 {
     private $temperature;
 
+    /**
+     * Weather constructor.
+     *
+     * @param float $temperature
+     */
     public function __construct(float $temperature)
     {
         $this->temperature = $temperature;
     }
 
+    /**
+     * Get temperature
+     *
+     * @return float
+     */
     public function getTemperature(): float
     {
         return $this->temperature;
